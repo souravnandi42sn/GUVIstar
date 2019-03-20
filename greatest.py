0be3f49add34ee17b29d9ffed3f0fdfe20c8ba6e
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-03-20 10:59:04 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,11 +1,11 @@
 try:
     n=input().split(" ")
-    if(int(n[0])>int(n[1])):
-        if(int(n[0])>int(n[2])):
+    if(int(n[0])>=int(n[1])):
+        if(int(n[0])>=int(n[2])):
             print(int(n[0]))
         else:
             print(int(n[2]))
-    elif(int(n[1])>int(n[2])):
+    elif(int(n[1])>=int(n[2])):
         print(int(n[1]))
     else:
         print(int(n[2]))
