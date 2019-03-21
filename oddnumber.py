ba3ccda734fe67733efc188cbeae66a71656053b
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-03-21 09:29:18 +0530

    Create oddnumber.py

---
+++ oddnumber.py
@@ -0,0 +1,18 @@
+'''
+Created on 20-Mar-2019
+
+@author: sourav nandi
+'''
+def isprime(n,m):
+    l=[]
+    for i in range(n+1,m):
+        if(i%2!=0):
+            l.append(i)
+    return(l)
+try:
+    n=input().split(" ")
+    l=isprime(int(n[0]),int(n[1]))
+    for m in l:
+        print(m,end=" ")
+except:
+    print("")
