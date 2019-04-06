42e4837a225422b908055dc2e3011922e1b82483
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-04-06 05:53:31 +0530

    Create unique.py

---
+++ unique.py
@@ -0,0 +1,21 @@
+try:
+    n=input()
+    m=input()
+    m=m[:int(n)]
+    c=[0 for i in range(int(n))]
+    for i in range(len(m)):
+        for j in range(i+1,int(n)):
+            if(m[i]==m[j]):
+                c[i]=c[i]+1
+    b=[]
+    print(c)
+    for i in range(int(n)):
+        if(int(c[i])>0):
+            b.append(int(m[i]))
+    b.sort()
+    g=len(b)
+    b=[str(b[i]) for i in range(g)]
+    b="".join(b)
+    print(b)
+except:
+    print("")
