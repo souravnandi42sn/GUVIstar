360e699b36bd1c818ac8c530f3f0e3012d0c3b48
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-04-06 05:55:57 +0530

    Update unique.py

--- unique.py
+++ unique.py
@@ -8,10 +8,11 @@ try:
             if(m[i]==m[j]):
                 c[i]=c[i]+1
     b=[]
-    print(c)
+    
     for i in range(int(n)):
         if(int(c[i])>0):
             b.append(int(m[i]))
+    b=list(set(b))
     b.sort()
     g=len(b)
     b=[str(b[i]) for i in range(g)]
