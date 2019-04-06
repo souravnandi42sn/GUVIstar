1d33d42a17348b7b0638bc0a05a225fcd0081de5
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-04-06 06:24:57 +0530

    Create larger.py

---
+++ larger.py
@@ -0,0 +1,14 @@
+try:
+    n=input()
+    m=input()
+    m=m[:int(n)]
+    c=[int(m[i]) for i in range(int(n))]
+    c.sort()
+    c=c[::-1]
+    b=[str(c[i]) for i in range(int(n))]
+    b="".join(b)
+    if(int(b)==0):
+        b=str(0)
+    print(b)
+except:
+    print(b)
